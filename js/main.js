7371785d5555ef8a1370c00f259a3dae089496cb
Marcin <marcin.adam.mroczko@gmail.com>
2021-02-06 12:01:49 +0100

    appear mobile menu

--- js/main.js
+++ js/main.js
@@ -1,7 +1,9 @@
 // Modules / classes
 import Slider from "./modules/Slider"
 import SliderVertical from "./modules/SliderVertical"
+import Nav from "./modules/Nav"
 
 // Instantiate a new object using modules/classes
 var slider = new Slider()
 var sliderVertical = new SliderVertical()
+var nav = new Nav()
---
+++ js/modules/Nav.js
@@ -0,0 +1,16 @@
+class Nav {
+  constructor() {
+    this.burger = document.querySelector(".burger")
+    this.mobileNav = document.querySelector(".header .nav")
+    this.toggleNav()
+  }
+
+  toggleNav() {
+    this.burger.addEventListener("click", () => {
+      this.mobileNav.classList.toggle("is-visible")
+      console.log("click")
+    })
+  }
+}
+
+export default Nav
