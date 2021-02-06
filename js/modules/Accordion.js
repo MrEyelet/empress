ae75696e6abcdd2aa3c5d945a7a6d0babd0fb1a5
Marcin <marcin.adam.mroczko@gmail.com>
2021-02-06 12:43:44 +0100

    toggle sloide accordion

--- js/modules/Accordion.js
+++ js/modules/Accordion.js
@@ -9,10 +9,10 @@ class Accordion {
       this.accBtn[i].addEventListener("click", function () {
         this.classList.toggle("active")
         var panel = this.nextElementSibling
-        if (panel.style.display === "block") {
-          panel.style.display = "none"
+        if (panel.style.maxHeight) {
+          panel.style.maxHeight = null
         } else {
-          panel.style.display = "block"
+          panel.style.maxHeight = panel.scrollHeight + "px"
         }
       })
     }
