43d3da295b7141d76a48e50cccf62f71bad00648
Marcin <marcin.adam.mroczko@gmail.com>
2021-02-07 00:12:07 +0100

    burger btn

--- js/modules/Nav.js
+++ js/modules/Nav.js
@@ -2,12 +2,14 @@ class Nav {
   constructor() {
     this.burger = document.querySelector(".burger")
     this.mobileNav = document.querySelector(".header .nav")
+    this.body = document.querySelector("body")
     this.toggleNav()
   }
 
   toggleNav() {
     this.burger.addEventListener("click", e => {
       this.mobileNav.classList.toggle("is-visible")
+      this.body.classList.toggle("is-blocked")
       e.target.classList.toggle("is-open")
     })
   }
