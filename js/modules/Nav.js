30f5f16b479a0bee5ba87888d3a3ab939a7d4d27
Marcin <marcin.adam.mroczko@gmail.com>
2021-02-08 00:15:39 +0100

    fade in menu links

--- js/modules/Nav.js
+++ js/modules/Nav.js
@@ -1,20 +1,10 @@
-import { gsap, TimelineMax, Power1, Power4 } from "gsap"
-gsap.registerPlugin()
-
 class Nav {
   constructor() {
     this.burger = document.querySelector(".burger")
     this.mobileNav = document.querySelector(".header .nav")
+    this.mobileNavEls = document.querySelectorAll(".nav__list-el")
     this.body = document.querySelector("body")
-    this.tl = new TimelineMax({})
-    this.toggleListEls()
     this.toggleNav()
-  }
-
-  toggleListEls() {
-    this.tl.from(".nav__list-el", { duration: 1, opacity: 0, y: "25px", stagger: 0.15, ease: Power1.easeOut })
-    this.tl.reverse(-1)
-    this.tl.reversed(true)
   }
 
   toggleNav() {
@@ -22,7 +12,9 @@ class Nav {
       this.mobileNav.classList.toggle("is-visible")
       this.body.classList.toggle("is-blocked")
       e.target.classList.toggle("is-open")
-      this.tl.reversed(!this.tl.reversed())
+      this.mobileNavEls.forEach(el => {
+        el.classList.toggle("fade-in")
+      })
     })
   }
 }
