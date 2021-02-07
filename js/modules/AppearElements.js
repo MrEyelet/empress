27ecc9d0874e7ca5693b9f5b00d2521544699360
Marcin <marcin.adam.mroczko@gmail.com>
2021-02-07 16:58:14 +0100

    hide illustration mobile

--- js/modules/AppearElements.js
+++ js/modules/AppearElements.js
@@ -27,10 +27,10 @@ class AppearElements {
     this.tlShowElements.from(".illustration-hero__rect", { duration: 1.5, scaleX: 0, stagger: 0.1, ease: Power4.easeOut }, "=-1.5")
     this.tlShowElements.from(".illustration-hero__circle", { duration: 1.5, opacity: 0, stagger: 0.1, ease: Power1.easeOut }, "=-1.2")
     this.tlShowElements.from(".illustration-hero__circle-group", { duration: 0.75, scale: 0, opacity: 0, transformOrigin: "center", ease: Power1.easeOut }, "=-1.7")
-    this.tlShowElements.from(".illustration-hero__inner", { duration: 0.75, scale: 1.2, opacity: 0, ease: Power4.easeOut }, "=-1.5")
+    this.tlShowElements.from(".illustration-hero__inner", { duration: 0.75, scale: 1.1, opacity: 0, ease: Power4.easeOut }, "=-1.5")
     setTimeout(() => {
       this.body.classList.remove("scroll-disabled")
-    }, 750)
+    }, 500)
     setTimeout(() => {
       this.preloader.remove()
       this.body.classList.remove("scroll-disabled")
