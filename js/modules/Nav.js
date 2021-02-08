ca6cc107178aeb8b52e61bba5cda55db1f7ca758
Marcin <marcin.adam.mroczko@gmail.com>
2021-02-08 12:46:55 +0100

    toggle class resize

--- js/modules/Nav.js
+++ js/modules/Nav.js
@@ -8,6 +8,11 @@ class Nav {
   }
 
   toggleNav() {
+    this.toggleClassesOnClick()
+    this.removeClassesonResize()
+  }
+
+  toggleClassesOnClick() {
     this.burger.addEventListener("click", e => {
       this.mobileNav.classList.toggle("is-visible")
       this.body.classList.toggle("is-blocked")
@@ -15,6 +20,19 @@ class Nav {
       this.mobileNavEls.forEach(el => {
         el.classList.toggle("fade-in")
       })
+    })
+  }
+  removeClassesonResize() {
+    window.addEventListener("resize", e => {
+      if (window.matchMedia("(min-width: 768px)").matches) {
+        this.body.classList.remove("is-blocked")
+        this.mobileNav.classList.remove("is-visible")
+        this.body.classList.remove("is-blocked")
+        this.burger.classList.remove("is-open")
+        this.mobileNavEls.forEach(el => {
+          el.classList.remove("fade-in")
+        })
+      }
     })
   }
 }
